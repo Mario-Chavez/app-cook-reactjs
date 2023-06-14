6f049a0d3601c911b858cab57cf882c897db00e6
Ana Rocha <anasoledad.rocha@gmail.com>
2023-06-14 00:58:25 -0300

    Se agregó el banner del componente inicio

---
+++ src/components/SegundoCarrusel.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const PrimerCarrusel = () => {
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100 carousel-responsive"
+            src="https://www.paulinacocina.net/wp-content/uploads/2023/03/flan-de-coco-4.jpg"
+            alt="Flan de coco"
+          />
+          <Carousel.Caption>
+            <h3 className="bg-dark opacity-75">Flan de coco</h3>
+
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100 carousel-responsive"
+            src="https://www.paulinacocina.net/wp-content/uploads/2023/02/pay-de-limon.jpg"
+            alt="Pay de limón"
+          />
+          <Carousel.Caption>
+            <h3 className="bg-dark opacity-75">Pay de limón</h3>
+
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={5000}>
+          <img
+            className="d-block w-100 carousel-responsive"
+            src="https://www.paulinacocina.net/wp-content/uploads/2022/05/receta-de-tarta-de-durazno.jpg"
+            alt="Tarta de durazno"
+          />
+          <Carousel.Caption>
+            <h3 className="bg-dark opacity-75">Tarta de durazno</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default PrimerCarrusel;
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Titulo from "../components/Titulo";
+import PrimerCarrusel from "../components/PrimerCarrusel";
+import SegundoCarrusel from "../components/SegundoCarrusel";
+import TercerCarrusel from "../components/TercerCarrusel";
 
 const Inicio = () => {
-    return (
-        <>
-            <Container className="my-5 mainPage">
-                <Titulo titulo="Soy  inicio " />
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container className="mainPage">
+      <Titulo titulo="Recetas de cocina fáciles y caseras" />
+        <Row className="p-4 banner mb-3">
+          <Col sm={7} md={8} className="px-0">
+            <PrimerCarrusel></PrimerCarrusel>
+          </Col>
+          <Col sm={5} md={4} className="mx-0">
+            <Row>
+              <Col md={12} className="px-0">
+                <SegundoCarrusel></SegundoCarrusel>
+              </Col>
+              <Col md={12} className="px-0">
+                <TercerCarrusel></TercerCarrusel>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Inicio;
