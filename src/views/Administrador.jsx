64f6bf5e1b74b0ccc84e3629c97f552a5be07c28
Mario-Chavez <marioachavez1@gmail.com>
2023-06-17 16:15:48 -0300

    se agrego el componente administrador

--- src/views/Administrador.jsx
+++ src/views/Administrador.jsx
@@ -1,7 +1,59 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { obtenerRecetas } from "../helpers/queries";
+// import { ItemReceta } from "../views/recetas/ItemReceta";
 const Administrador = () => {
-    return <div></div>;
+    const [recetas, setRecetas] = useState([]);
+
+    useEffect(() => {
+        obtenerRecetas().then((respuesta) => {
+            console.log(respuesta);
+            setRecetas(respuesta);
+            // todo: resolvcer el error si lo huibiera en coneccion en API
+        });
+    }, []);
+
+    return (
+        <Container className="mainPage">
+            <div className="row mt-5">
+                <div className="display-4 col-8">
+                    <h1>Nuestras Recetas</h1>
+                </div>
+                <div className="col-4 d-flex justify-content-end">
+                    <Link
+                        type="button"
+                        className="btn btn-outline-warning"
+                        to={"/administrador/crear"}
+                    >
+                        Agregar Receta
+                    </Link>
+                </div>
+            </div>
+            <hr />
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Codigo</th>
+                        <th scope="col">Nombre Receta</th>
+                        <th scope="col">categoria</th>
+                        <th scope="col">ingredientes</th>
+
+                        <th scope="col">imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* {recetas.map((receta) => (
+                        <ItemReceta
+                            key={receta.id}
+                            receta={receta}
+                            setRecetas={setRecetas}
+                        ></ItemReceta>
+                    ))} */}
+                </tbody>
+            </table>
+        </Container>
+    );
 };
 
 export default Administrador;
